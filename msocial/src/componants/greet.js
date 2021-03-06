5df1603bdf3e2174929c3b621f2333f6355d4778
Satish7897 <55326969+Satish7897@users.noreply.github.com>
2021-03-06 13:46:05 +0530

    Create greet.js

---
+++ msocial/src/componants/greet.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+const greet = ({name,classType}) => {
+    return (<div>
+        <h1>hello world {name} {classType}</h1>
+        </div>);
+    
+}
+export default greet;
