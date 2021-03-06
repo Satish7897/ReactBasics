0eec96a8f450525a56324a55d503fd324b1b9c84
Satish7897 <55326969+Satish7897@users.noreply.github.com>
2021-03-06 13:45:46 +0530

    Create counterUsingFunctional.js

---
+++ msocial/src/componants/counterUsingFunctional.js
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(100)
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  )
+}
+export default Example
